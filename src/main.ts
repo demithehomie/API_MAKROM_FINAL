f74d96dd149f16820a7fc6aa0cdaf909d34535f6
Demetrius Ferreira <demithehomie@gmail.com>
2023-05-24 16:18:16 -0300

    Deploy_3

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'http://localhost:8100',
+      'https://coopeere-frontend.web.app', 
 
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
@@ -21,7 +21,7 @@ async function bootstrap() {
 
   app.use(
     cors({
-      origin: ['*', 'http://localhost:8100'],
+      origin: ['*', 'https://coopeere-frontend.web.app'],
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       allowedHeaders: ['Content-Type', 'Authorization'],
     }),
