115061d1980cd19c0f6a0007c17ab563a03d8f60
Demetrius Ferreira <demithehomie@gmail.com>
2023-06-05 09:09:06 -0300

    Apagando Migrations

--- prisma/migrations/20230530105824_first_relations/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-CREATE SEQUENCE prodist_idcooperado_seq;
-ALTER TABLE "Prodist" ALTER COLUMN "idCooperado" SET DEFAULT nextval('prodist_idcooperado_seq');
-ALTER SEQUENCE prodist_idcooperado_seq OWNED BY "Prodist"."idCooperado";
